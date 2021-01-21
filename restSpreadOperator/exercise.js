7860354be9aca905bdb008c73c0c7c54c7ccc01a
Daniel Knapp <knapparatus@gmail.com>
2021-01-21 12:24:10 -0600

    rest/spread exercises complete

--- restSpreadOperator/exercise.js
+++ restSpreadOperator/exercise.js
@@ -3,20 +3,18 @@
 // In this exercise, you’ll refactor some ES5 code into ES2015.
 // Given this function:
 
-function filterOutOdds() {
-	var nums = Array.prototype.slice.call(arguments);
-	return nums.filter(function(num) {
-		return num % 2 === 0;
-	});
-}
+// function filterOutOdds() {
+// 	var nums = Array.prototype.slice.call(arguments);
+// 	return nums.filter(function(num) {
+// 		return num % 2 === 0;
+// 	});
+// }
 
 // Refactor it to use the rest operator & an arrow function:
 
 /* Write an ES2015 Version */
 
-function filterOutOdds(...nums) {
-	return nums.filter(num => num % 2 === 0);
-}
+const filterOutOdds = (...nums) => nums.filter(num => num % 2 === 0);
 
 // findMin
 
@@ -24,22 +22,31 @@ function filterOutOdds(...nums) {
 
 // Make sure to do this using the rest and spread operator.
 
-// findMin(1,4,12,-3) // -3
-// findMin(1,-1) // -1
-// findMin(3,1) // 1
+const findMin = (...values) => Math.min(...values);
+
+findMin(1, 4, 12, -3); // -3
+findMin(1, -1); // -1
+findMin(3, 1); // 1
 
 // mergeObjects
 
 // Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 
-// mergeObjects({a:1, b:2}, {c:3, d:4}) // {a:1, b:2, c:3, d:4}
+const mergeObjects = (a, b) => ({
+	...a,
+	...b
+});
+
+mergeObjects({ a: 1, b: 2 }, { c: 3, d: 4 }); // {a:1, b:2, c:3, d:4}
 
 // doubleAndReturnArgs
 
 // Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments. The function should return a new array with the original array values and all of additional arguments doubled.
 
-// doubleAndReturnArgs([1,2,3],4,4) // [1,2,3,8,8]
-// doubleAndReturnArgs([2],10,4) // [2, 20, 8]
+const doubleAndReturnArgs = (arr, ...args) => [ ...arr, ...args.map(vals => vals * 2) ];
+
+doubleAndReturnArgs([ 1, 2, 3 ], 4, 4); // [1,2,3,8,8]
+doubleAndReturnArgs([ 2 ], 10, 4); // [2, 20, 8]
 
 // Slice and Dice!
 
@@ -50,25 +57,45 @@ function filterOutOdds(...nums) {
 /** remove a random element in the items array
 and return a new array without that item. */
 
-function removeRandom(items) {}
+const removeRandom = items => {
+	const idx = Math.floor(Math.random() * items.length);
+	return [ ...items.slice(0, idx), ...items.slice(idx + 1) ];
+};
+
+const randomItems = [ 10, 'el', 18, 67, 'hello' ];
 
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {}
+const extend = (array1, array2) => [ ...array1, ...array2 ];
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {}
+const addKeyVal = (obj, key, val) => {
+	const newObj = { ...obj };
+	newObj[key] = val;
+	return newObj;
+};
+
+addKeyVal({ name: 'daniel', age: 23 }, 'height', '5.6');
 
 /** Return a new object with a key removed. */
 
-function removeKey(obj, key) {}
+const removeKey = (obj, key) => {
+	delete obj[key];
+	return { ...obj };
+};
+
+removeKey({ name: 'daniel', age: 23, height: '5.6' }, 'height');
 
 /** Combine two objects and return a new object. */
 
-function combine(obj1, obj2) {}
+const combine = (obj1, obj2) => ({ ...obj1, ...obj2 });
+
+combine({ name: 'daniel', age: 23, height: '5.6' }, { job: 'engineer', username: 'boo' });
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {}
+const update = (obj, key, val) => ({ ...obj, [key]: val });
+
+update({ name: 'daniel', age: 23, height: '5.6' }, 'name', 'engineer');
